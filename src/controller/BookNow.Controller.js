13a93a2491cf618e932b83b59f2ac3698378db38
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-05-30 00:18:31 +0530

    Get route create

--- src/controller/BookNow.Controller.js
+++ src/controller/BookNow.Controller.js
@@ -1,6 +1,6 @@
 import createHttpError from "http-errors";
 import UserBookNow from "../models/BookNow.model.js";
-
+import mongoose from "mongoose";
 const BookNowData = async (req, res, next) => {
     const { pickUpLocation, dropOffLocation, distance } = req.body;
 
@@ -44,4 +44,53 @@ const BookNowData = async (req, res, next) => {
     }
 }
 
-export default BookNowData;
+const BookNowDataDriver = async (req, res, next) => {
+    try {
+      
+      const bookings = await UserBookNow.find();
+  
+      
+      res.status(200).json({
+        message: "Booking data retrieved successfully",
+        bookings: bookings
+      });
+    } catch (error) {
+    
+      const err = createHttpError(500, "Error occurred when retrieving booking data");
+      return next(err);
+    }
+  };
+
+  const singleBookdata = async (req, res, next) => {
+    try {
+      
+      const { id } = req.params;
+      console.log("Extracted ID:", id);
+  
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(createHttpError(400, "Invalid booking ID format"));
+      }
+  
+      
+      const booking = await UserBookNow.findById(id);
+      console.log("Found booking:", booking);
+  
+      
+      if (!booking) {
+        return next(createHttpError(404, "Booking not found"));
+      }
+  
+      // Send successful response with the booking details
+      res.status(200).json({
+        message: "Booking data retrieved successfully",
+        booking: booking
+      });
+    } catch (error) {
+      console.error("Error:", error); // Log the actual error object
+      const err = createHttpError(500, "Error occurred when retrieving booking data");
+      return next(err);
+    }
+  };
+  
+
+export {BookNowData,BookNowDataDriver,singleBookdata};
--- src/routes/BookNow/Booknow.route.js
+++ src/routes/BookNow/Booknow.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import BookNowData from '../../controller/BookNow.Controller.js';
+import {BookNowData,BookNowDataDriver,singleBookdata} from '../../controller/BookNow.Controller.js';
 
 const BookNowRouter = express.Router();
 
 
 BookNowRouter.post('/', BookNowData);
+BookNowRouter.get('/BookNowDataDriver', BookNowDataDriver);//get all booking data
+BookNowRouter.get('/BookNowDataDriver/:id',singleBookdata)
+
 
 export default BookNowRouter;
